dc45ac1d4b74c2b55f98f1f4ce76fe04c8a52e74
Nguyen Khoa <116504460+kztera@users.noreply.github.com>
2023-07-09 13:58:28 +0700

    feat: add content for instruct_screen

--- lib/screens/instruct_screen.dart
+++ lib/screens/instruct_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'components/share_appbar.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class InstructScreen extends StatefulWidget {
   const InstructScreen({super.key});
@@ -22,9 +22,79 @@ class _InstructScreenState extends State<InstructScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: SharedAppBar(
-        title: AppLocalizations.of(context).titleAppBar,
+        title: AppLocalizations.of(context).titleHowToUse,
         onLanguageChanged: handleLanguageChanged,
+        nameScreen: "instruct_screen",
+      ),
+      body: SafeArea(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // verticalDirection: VerticalDirection.down,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50, bottom: 50),
+                child: Text(AppLocalizations.of(context).instruct_0,
+                    style: GoogleFonts.patrickHand(
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center),
+              ),
+              Container(
+                height: 450,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context).instruct_1,
+                      style: GoogleFonts.patrickHand(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      AppLocalizations.of(context).instruct_2,
+                      style: GoogleFonts.patrickHand(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      AppLocalizations.of(context).instruct_3,
+                      style: GoogleFonts.patrickHand(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      AppLocalizations.of(context).instruct_4,
+                      style: GoogleFonts.patrickHand(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      AppLocalizations.of(context).instruct_5,
+                      style: GoogleFonts.patrickHand(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Expanded(
+                child: Image(image: AssetImage('assets/images/flork-love.png')),
+              )
+            ]),
       ),
     );
   }
